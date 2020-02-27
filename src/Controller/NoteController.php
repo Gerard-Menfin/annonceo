40b419b6eae2be2bd436479859e06d51ef7f6db9
unknown <stagiaire@B-6-F3R9J32.b-doranco.prod>
2020-02-27 13:47:00 +0100

    Upload photos - Note

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\MembreRepository as MR;
+use Doctrine\ORM\EntityManagerInterface as EMI;
+use Symfony\Component\HttpFoundation\Request;
 use App\Form\AttribuerNoteType;
 
 class NoteController extends AbstractController
@@ -22,9 +24,32 @@ class NoteController extends AbstractController
     /**
      * @Route("/profil/attribuer-note/{pseudo}", name="attribuer_note")
      */
-    public function attribuer(MR $mr, $pseudo){
+    public function attribuer(MR $mr, Request $rq, EMI $em, $pseudo){
+        if($pseudo == $this->getUser()->getPseudo()){
+            $this->addFlash("error", "Vous ne pouvez pas vous noter vous-même, petit salopiaud !");
+            return $this->redirectToRoute("profil");
+        }
+
         $membre = $mr->findOneBy([ "pseudo" => $pseudo ]);
+
         $form = $this->createForm(AttribuerNoteType::class);
+        $form->handleRequest($rq);
+        if($form->isSubmitted()){
+            if($form->isValid()){
+                $note = $form->getData();
+                $note->setMembreNotant($this->getUser());
+                $note->setDateEnregistrement(new \DateTime());
+                $note->setMembreNote($membre);
+                $em->persist($note);
+                $em->flush();
+                $this->addFlash("success", "Votre note a bien été prise en compte");
+                return $this->redirectToRoute("profil");
+            }
+            else{
+                $this->addFlash("error", "Une erreur est survenu !");
+            }
+        }
+
         $form = $form->createView();
         return $this->render("note/attribuer.html.twig", compact("membre", "form"));
     }
--- src/Form/AttribuerNoteType.php
+++ src/Form/AttribuerNoteType.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Form;
 
 use App\Entity\Note;
@@ -7,13 +6,26 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type as Input;
+use Symfony\Component\Validator\Constraints;
+
+
 class AttribuerNoteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('note')
+            ->add('note', 
+                  Input\NumberType::class, 
+                  [ "attr" => [ "min" => 1, "max" => 5 ],
+                    "constraints" => [ 
+                        new Constraints\NotBlank(["message" => "Vous devez attribuer une note"]),
+                        new Constraints\LessThanOrEqual([ "value" => 5, "message" => "La note doit être inférieure ou égale à 5" ]),
+                        new Constraints\GreaterThan([ "value" => 0, "message" => "La note doit être supérieure à 0" ])
+                    ]
+                  ])
             ->add('avis')
+            ->add("noter", Input\SubmitType::class, [ "label" => "Noter"])
             // ->add('date_enregistrement')
             // ->add('membre_notant')
             // ->add('membre_note')
